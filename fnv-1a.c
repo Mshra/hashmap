aa7c4b006435e0490010de6dfd1cf85acaddf88e
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-05-12 22:51:34 +0530

    optimized

--- fnv-1a.c
+++ fnv-1a.c
@@ -8,29 +8,27 @@ const uint32_t FNV_32_PRIME = 0x1000193;   // 16777619
 const uint64_t FNV_64_OFFSET = 0xcbf29ce484222325; // 14695981039346656037
 const uint64_t FNV_64_PRIME = 0x100000001b3;       // 1099511628211U
 
-uint32_t FNV_ONEA_32(char *string) {
+static uint32_t FNV_ONEA_32(char *string) {
   uint8_t byte_of_data = 8;
   uint32_t hash = FNV_32_OFFSET;
 
   while (*string) {
     int c = *string;
-    hash = hash ^ c;
-    hash = hash * FNV_32_PRIME;
-
+    hash ^= c;
+    hash *= FNV_32_PRIME;
     string = string + 1;
   }
   return hash;
 }
 
-uint64_t FNV_ONEA_64(char *string) {
+static uint64_t FNV_ONEA_64(char *string) {
   uint8_t byte_of_data = 8;
   uint64_t hash = FNV_64_OFFSET;
 
   while (*string) {
     int c = *string;
-    hash = hash ^ c;
-    hash = hash * FNV_64_PRIME;
-
+    hash ^= c;
+    hash *= FNV_64_PRIME;
     string = string + 1;
   }
   return hash;
