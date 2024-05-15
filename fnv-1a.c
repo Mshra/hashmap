950021f6c17215ee7862ae4952287cc54979cbea
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-05-15 09:39:23 +0530

    does hashing

--- fnv-1a.c
+++ fnv-1a.c
@@ -1,6 +1,12 @@
-/* implementation of FNV-1A HASH */
-
 #include <stdint.h>
+
+/*
+ * This C file implements the Fowler-Noll-Vo (FNV) hash algorithm variant 1a,
+ * which is a non-cryptographic hash function. It iterates through each
+ * character of the input string, XORing the hash with the character's value and
+ * then multiplying by a prime number. This process is repeated for each
+ * character in the string.
+ */
 
 const uint32_t FNV_32_OFFSET = 0x811c9dc5; // 2166136261
 const uint32_t FNV_32_PRIME = 0x1000193;   // 16777619
@@ -8,28 +14,42 @@ const uint32_t FNV_32_PRIME = 0x1000193;   // 16777619
 const uint64_t FNV_64_OFFSET = 0xcbf29ce484222325; // 14695981039346656037
 const uint64_t FNV_64_PRIME = 0x100000001b3;       // 1099511628211U
 
+/**
+ * @brief Calculate the FNV-1a 32-bit hash of a given string.
+ *
+ * @param string The input string for which the hash is to be calculated.
+ * @return The 32-bit hash value of the input string.
+ */
 static uint32_t FNV_ONEA_32(char *string) {
-  uint8_t byte_of_data = 8;
-  uint32_t hash = FNV_32_OFFSET;
+  uint32_t hash = FNV_32_OFFSET; // Initialize the hash with FNV offset basis.
 
+  // Iterate through each character of the input string.
   while (*string) {
-    int c = *string;
-    hash ^= c;
-    hash *= FNV_32_PRIME;
-    string = string + 1;
+    int c = *string;      // Get the ASCII value of the current character.
+    hash ^= c;            // XOR the hash with the ASCII value of the character.
+    hash *= FNV_32_PRIME; // Multiply the hash by the FNV prime constant.
+    string = string + 1;  // Move to the next character in the string.
   }
-  return hash;
+
+  return hash; // Return the final hash value.
 }
 
+/**
+ * @brief Calculate the FNV-1a 64-bit hash of a given string.
+ *
+ * @param string The input string for which the hash is to be calculated.
+ * @return The 64-bit hash value of the input string.
+ */
 static uint64_t FNV_ONEA_64(char *string) {
-  uint8_t byte_of_data = 8;
-  uint64_t hash = FNV_64_OFFSET;
+  uint64_t hash = FNV_64_OFFSET; // Initialize the hash with FNV offset basis.
 
+  // Iterate through each character of the input string.
   while (*string) {
-    int c = *string;
-    hash ^= c;
-    hash *= FNV_64_PRIME;
-    string = string + 1;
+    int c = *string;      // Get the ASCII value of the current character.
+    hash ^= c;            // XOR the hash with the ASCII value of the character.
+    hash *= FNV_64_PRIME; // Multiply the hash by the FNV prime constant.
+    string = string + 1;  // Move to the next character in the string.
   }
-  return hash;
+
+  return hash; // Return the final 64-bit hash value.
 }
