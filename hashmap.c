69ffd58b76747b38b0d1bda74f49cead15eed204
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-05-21 15:56:22 +0530

    1.0

--- hashmap.c
+++ hashmap.c
@@ -4,51 +4,60 @@
 #include <stdlib.h>
 
 int get_index(char *key) { return (FNV_ONEA_64(key) % 16); }
-//
-// void insert(char *hash_map[], char *key, int value) {
-//   *hash_map[get_index(key)] = value;
-// }
 
-// Item is a <key-value> pair of a hash_map
 typedef struct {
   char *key;
   int32_t value;
 } Item;
 
+typedef struct {
+  int length;
+  Item map[];
+} Hash_Map;
+
 const Item NULL_ITEM = {NULL, 0};
 
-void print_item(Item item) { printf("\t{ %s, %d },\n", item.key, item.value); }
+void print_item(Item item) { printf("  { %s, %d },\n", item.key, item.value); }
 
-void insert(Item map[], int length, char *key, int32_t value) {
+void print_hmap(Hash_Map *hmap) {
+  printf("{\n");
+  for (int i = 0; i < hmap->length; i++) {
+    if (sizeof(hmap->map[i]) != sizeof(Item))
+      print_item(NULL_ITEM);
+    else
+      print_item(hmap->map[i]);
+  }
+  printf("}\n");
+}
+
+void insert(Hash_Map *hmap, char *key, int32_t value) {
   Item tmp_item = {key, value};
-  map[get_index(key)] = tmp_item;
+  hmap->map[get_index(key)] = tmp_item;
+}
+
+Hash_Map *create_hashmap(int length) {
+  Hash_Map *tmp_map = (Hash_Map *)malloc(sizeof(Hash_Map));
+  tmp_map->length = length;
+
+  for (int i = 0; i < length; i++) {
+    tmp_map->map[i] = NULL_ITEM;
+  }
+  return tmp_map;
 }
 
 int main(int argc, char *argv[]) {
   int length = 16;
-  Item map[] = {
-      NULL_ITEM, NULL_ITEM, NULL_ITEM, NULL_ITEM, NULL_ITEM, NULL_ITEM,
-      NULL_ITEM, NULL_ITEM, NULL_ITEM, NULL_ITEM, NULL_ITEM, NULL_ITEM,
-      NULL_ITEM, NULL_ITEM, NULL_ITEM, NULL_ITEM,
-  };
 
-  insert(map, length, "bar", 42);
-  insert(map, length, "jane", 100);
-  insert(map, length, "foo", 10);
-  insert(map, length, "bazz", 36);
-  insert(map, length, "buzz", 7);
-  insert(map, length, "bob", 11);
+  Hash_Map *hmap = create_hashmap(16);
 
-  printf("{\n");
-  for (int i = 0; i < length; i++) {
-    if (sizeof(map[i]) != 16)
-      print_item(NULL_ITEM);
-    else
-      print_item(map[i]);
-  }
+  insert(hmap, "bar", 42);
+  insert(hmap, "jane", 100);
+  insert(hmap, "foo", 10);
+  insert(hmap, "bazz", 36);
+  insert(hmap, "buzz", 7);
+  insert(hmap, "bob", 11);
 
-  printf("}\n");
-  printf("hello, world\n");
+  print_hmap(hmap);
 
   return EXIT_SUCCESS;
 }
